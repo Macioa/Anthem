ac6a9e9c93e19a0d5f4d3cea712362f1fcab1501
macioa <macioa.macioa@gmail.com>
2021-01-21 16:09:16 -0600

    add specs and refactor

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -18,52 +18,30 @@ export class GildedRose {
     }
 
     updateQuality() {
-        for (let i = 0; i < this.items.length; i++) {
-            if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-                if (this.items[i].quality > 0) {
-                    if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                        this.items[i].quality = this.items[i].quality - 1
-                    }
-                }
-            } else {
-                if (this.items[i].quality < 50) {
-                    this.items[i].quality = this.items[i].quality + 1
-                    if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-                        if (this.items[i].sellIn < 11) {
-                            if (this.items[i].quality < 50) {
-                                this.items[i].quality = this.items[i].quality + 1
-                            }
-                        }
-                        if (this.items[i].sellIn < 6) {
-                            if (this.items[i].quality < 50) {
-                                this.items[i].quality = this.items[i].quality + 1
-                            }
-                        }
-                    }
-                }
+        this.items = this.items.map(({name, sellIn, quality})=>{
+            let qualityDelta = (sellIn>0) ? -1 : -2 // Quality Degredation Rate
+            let sellInDelta = -1 // SellIn Days Rate
+            switch(name){
+                case 'Aged Brie':
+                    qualityDelta = 1
+                    break
+                case 'Sulfuras, Hand of Ragnaros':
+                    qualityDelta = 0
+                    sellInDelta = 0
+                    break
+                case 'Backstage passes to a TAFKAL80ETC concert':
+                    qualityDelta = 1
+                    if (sellIn <= 10) qualityDelta++
+                    if (sellIn <= 5) qualityDelta++
+                    if (sellIn <= 0) qualityDelta = -quality
+                    break
             }
-            if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].sellIn = this.items[i].sellIn - 1;
-            }
-            if (this.items[i].sellIn < 0) {
-                if (this.items[i].name != 'Aged Brie') {
-                    if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-                        if (this.items[i].quality > 0) {
-                            if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                                this.items[i].quality = this.items[i].quality - 1
-                            }
-                        }
-                    } else {
-                        this.items[i].quality = this.items[i].quality - this.items[i].quality
-                    }
-                } else {
-                    if (this.items[i].quality < 50) {
-                        this.items[i].quality = this.items[i].quality + 1
-                    }
-                }
-            }
-        }
-
+            sellIn += sellInDelta
+            quality += qualityDelta
+            quality = Math.max(quality,0)
+            quality = Math.min(quality,50)
+            return new Item(name, sellIn, quality)
+        })
         return this.items;
     }
 }
--- TypeScript/test/gilded-rose.spec.ts
+++ TypeScript/test/gilded-rose.spec.ts
@@ -1,12 +1,94 @@
 import { expect } from 'chai';
 import { Item, GildedRose } from '../app/gilded-rose';
 
+/*
+	- All items have a SellIn value which denotes the number of days we have to sell the item
+	- All items have a Quality value which denotes how valuable the item is
+	- At the end of each day our system lowers both values for every item
+
+Pretty simple, right? Well this is where it gets interesting:
+
+	- Once the sell by date has passed, Quality degrades twice as fast
+	- The Quality of an item is never negative
+	- "Aged Brie" actually increases in Quality the older it gets
+	- The Quality of an item is never more than 50
+	- "Sulfuras", being a legendary item, never has to be sold or decreases in Quality
+	- "Backstage passes", like aged brie, increases in Quality as its SellIn value approaches;
+	Quality increases by 2 when there are 10 days or less and by 3 when there are 5 days or less but
+    Quality drops to 0 after the concert
+*/
+
 describe('Gilded Rose', function () {
 
     it('should foo', function() {
         const gildedRose = new GildedRose([ new Item('foo', 0, 0) ]);
         const items = gildedRose.updateQuality();
-        expect(items[0].name).to.equal('fixme');
+        expect(items[0].name).to.equal('foo');
     });
 
+//**** CORE REQ */
+    it('should lower sellIn', function() {
+        const gildedRose = new GildedRose([ new Item('foo', 5, 5) ]);
+        const items = gildedRose.updateQuality();
+        expect(items[0].sellIn).to.equal(4);
+    });
+
+    it('should lower quality', function() {
+        const gildedRose = new GildedRose([ new Item('foo', 5, 5) ]);
+        const items = gildedRose.updateQuality();
+        expect(items[0].quality).to.equal(4);
+    });
+
+//**** SPECIAL REQ*/
+    it('should double degradation after sell date', function() {
+        const gildedRose = new GildedRose([ new Item('foo', -1, 2) ]);
+        const items = gildedRose.updateQuality();
+        expect(items[0].quality).to.equal(0);
+    });
+
+    it('should not give negative quality', function() {
+        const gildedRose = new GildedRose([ new Item('foo', 1, 0) ]);
+        const items = gildedRose.updateQuality();
+        expect(items[0].quality).to.be.gte(0);
+    });
+
+
+    it('should increase aged brie quality', function() {
+        const gildedRose = new GildedRose([ new Item('Aged Brie', 1, 0) ]);
+        const items = gildedRose.updateQuality();
+        expect(items[0].quality).to.equal(1);
+    });
+
+    it('should not raise quality over 50', function() {
+        const gildedRose = new GildedRose([ new Item('foo', 5, 50) ]);
+        const items = gildedRose.updateQuality();
+        expect(items[0].quality).to.be.lte(50);
+    });
+
+    it('should not alter Sulfuras quality', function() {
+        const gildedRose = new GildedRose([ new Item('Sulfuras, Hand of Ragnaros', 5, 5) ]);
+        const items = gildedRose.updateQuality();
+        expect(items[0].quality).to.equal(5);
+        expect(items[0].sellIn).to.equal(5);
+    });
+
+    describe('Backstage passes', function () {
+        it("should increase quality by 2 when there are 10 days or less", function() {
+            const gildedRose = new GildedRose([ new Item('Backstage passes to a TAFKAL80ETC concert', 10, 5) ]);
+            const items = gildedRose.updateQuality();
+            expect(items[0].quality).to.equal(7);
+        });
+
+        it("should increase quality by 3 when there are 5 days or less", function() {
+            const gildedRose = new GildedRose([ new Item('Backstage passes to a TAFKAL80ETC concert', 5, 5) ]);
+            const items = gildedRose.updateQuality();
+            expect(items[0].quality).to.equal(8);
+        });
+
+        it("should set quality to 0 after concert", function() {
+            const gildedRose = new GildedRose([ new Item('Backstage passes to a TAFKAL80ETC concert', 0, 50) ]);
+            const items = gildedRose.updateQuality();
+            expect(items[0].quality).to.equal(0);
+        });
+    })
 });
